7b354e04b4402cb216956dc5830fa4ec2210c68a
laohu <laohubbb@gmail.com>
2022-09-03 14:55:32 +0800

    添加表单验证

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const express=require('express')
 
 const app=express()
 
-
 const cors= require('cors')
 app.use(cors())
 
 app.use(express.urlencoded({extended:false}))
 
 app.use((req,res,next)=>{
-    res.cc =function (err,status=1){
+    res.cc =function (msg,status=1){
         res.send({
             status,
-            message:err instanceof Error ? err.message :err,
+            message:msg instanceof Error ? msg.message :msg,
         })
     }
     next()
 })
 
+const joi = require('joi')
+
+
+
 const userRouter=require('./router/user')
 app.use('/api',userRouter)
+app.use( (err,req,res,next)=> {
+    if (err instanceof joi.ValidationError)  res.cc(err)
+    res.cc(err)
+})
 
 app.listen(3006,()=>{
     console.log('api server runnning at http://127.0.0.1:3007')
--- router/user.js
+++ router/user.js
@@ -4,7 +4,12 @@ const router=express.Router()
 
 const userHandler=require('../router-handler/user')
 
-router.post('/reguser',userHandler.regUser)
+
+const expressJoi = require('@escook/express-joi')
+const { reg_login_schema } = require('../schema/user')
+
+
+router.post('/reguser',expressJoi(reg_login_schema),userHandler.regUser)
 
 router.post('/login',userHandler.login)
 
